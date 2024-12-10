7bcd26b4436af48cf54ab8d45c8aced9bc683986
najiashaiq <najiashaiq786@gmail.com>
2024-12-09 22:14:55 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { Navbar } from "./app/Navbar";
+import { AddPostForm } from "./features/posts/AddPostForm";
+import { PostsList } from "./features/posts/PostsList";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <>
+              <AddPostForm />
+              <PostsList />
+            </>
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
